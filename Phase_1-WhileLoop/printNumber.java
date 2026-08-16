4d8b1e5df86f05593b554b522770647f4dd9ffb3
vaaibhavkaushik <vaibhavkaushik2611@gmail.com>
2026-08-16 20:07:32 +0530

    Add first Java DSA program

---
+++ Phase_1-WhileLoop/printNumber.java
@@ -0,0 +1,11 @@
+// 1. Print all numbers from 1 to 10 using a loop.
+
+public class printNumber {
+    public static void main(String[] args) {
+        int n = 1;
+        while(n <= 10) {
+            System.out.println(n);
+            n++;
+        }
+    }
+}
