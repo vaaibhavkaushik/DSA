23dad03c6b5535cba596ffdf4ff8a4f06944b38d
vaaibhavkaushik <vaibhavkaushik2611@gmail.com>
2026-08-16 20:23:28 +0530

    Add reverse number while loop program

---
+++ Phase_1-WhileLoop/printNumber_rev.java
@@ -0,0 +1,10 @@
+// 2. Print numbers from 10 down to 1 in reverse order.
+public class printNumber_rev {
+    public static void main (String[] args) {
+        int n=10;
+        while(n>=1) {
+            System.out.println(n);
+            n--;
+        }
+    }
+}
